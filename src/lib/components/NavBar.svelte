6a7278c177aedace68609d740bea801fb571e9b0
asimsedhain <asimsedhain33@gmail.com>
2023-09-03 13:03:07 -0500

    fix: remove unseen go to content button

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -1,12 +1,10 @@
 <script lang="ts">
 	import { base } from "$app/paths";
-	let header: HTMLElement;
 </script>
 
-<header bind:this={header} tabindex="-1">
+<header tabindex="-1">
 	<div>
 		<nav>
-			<a href="#top" class="skip-to-content">Skip to content</a>
 			<ul>
 				<li><a href="{base}/">Home</a></li>
 				<li><a href="{base}/writings">Writings</a></li>
